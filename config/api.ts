373aefe6a7f9cc604ab6cfdbe6bb1ddc2209751e
Thisaru632 <thisarudilhara2@gmail.com>
2026-02-19 10:48:43 +0530

    Update api.ts

--- config/api.ts
+++ config/api.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:5000';
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://senu-backend-xu6s.vercel.app';
 
 export const API_ENDPOINTS = {
     BOOKINGS: `${API_BASE_URL}/api/bookings`,
