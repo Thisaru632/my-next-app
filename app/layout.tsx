cd2086baf88e2dc0b240aff2021f61186af1df53
Thisaru632 <thisarudilhara2@gmail.com>
2026-06-05 16:07:54 +0530

    feat: initialize root layout with providers, analytics tracking, and global UI components

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,7 +52,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '2419987841842346');
+            fbq('init', '2258619591220145');
             fbq('track', 'PageView');
           `}
         </Script>
@@ -64,7 +64,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
             height="1" 
             width="1" 
             style={{ display: 'none' }} 
-            src="https://www.facebook.com/tr?id=2419987841842346&ev=PageView&noscript=1" 
+            src="https://www.facebook.com/tr?id=2258619591220145&ev=PageView&noscript=1" 
           />
         </noscript>
         <UserProvider>
